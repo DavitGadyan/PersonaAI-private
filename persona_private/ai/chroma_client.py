2b3900a721ba36ee46e72a5548715d6134b793e1
David <davidgadyan92@gmail.com>
2024-09-17 13:40:25 +0200

    update

--- persona_private/ai/chroma_client.py
+++ persona_private/ai/chroma_client.py
@@ -83,7 +83,7 @@ def load_json(json_obj, filename):
     loader = StringLoader(json_text, metadata={"source": filename})
     documents = loader.load()
 
-    text_splitter = CharacterTextSplitter(chunk_size=2052, chunk_overlap=50, separator="<END-END>")
+    text_splitter = CharacterTextSplitter(chunk_size=2052, chunk_overlap=0, separator="<END-END>")
     documents = text_splitter.split_documents(documents)
 
     return documents
