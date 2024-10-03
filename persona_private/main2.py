1fcbf242ab4da98e6a4e8d9f383f6f83002183a1
David <davidgadyan92@gmail.com>
2024-10-03 23:26:43 +0800

    mongodb update

---
+++ persona_private/main2.py
@@ -0,0 +1,41 @@
+from pymongo import MongoClient
+
+# Create a connection to the MongoDB server
+client = MongoClient('mongodb://localhost:27017/')
+
+# Connect to the database
+db = client['docs']
+
+# Connect to the collection
+collection = db['jsons']
+
+# Execute the query to extract the top 3 countries with the highest 'moi' value
+results = collection.aggregate([
+    { "$unwind": "$moi" },
+    {
+        "$addFields": {
+            "moi": {
+                "$toDouble": {
+                    "$arrayElemAt": [
+                        { "$split": ["$moi.FileAttachmentString", " "] },
+                        # index of the numeric value in the split array
+                    ]
+                }
+            }
+        }
+    },
+    {
+        "$group": {
+            "_id": "$CountryName",
+            "moi": { "$avg": "$moi" }
+        }
+    },
+    { "$sort": { "moi": -1 } },
+    { "$limit": 3 },
+    { "$project": { "_id": 0, "CountryName": "$_id", "moi": 1 } }
+])
+
+# Print the results
+print("The top 3 countries with the highest 'moi' value are:")
+for i, result in enumerate(results):
+    print(f"{i+1}. {result['CountryName']} ({result['moi']})")
