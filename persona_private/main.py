342b4fbad386b43116aa83662fa32066ae95d70a
David <davidgadyan92@gmail.com>
2024-10-06 00:09:42 +0800

    update main file

--- persona_private/main.py
+++ persona_private/main.py
@@ -2,7 +2,7 @@ import json
 from persona_private.mongodb.client import insert_data
 
 # Open and read the JSON file
-with open('ai/files/file-2024.08.20.13.28.json', 'r') as file:
+with open('ai/files/data_cleaned_05.10.24.json', 'r') as file:
     data = json.load(file)
 
 
